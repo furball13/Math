5ecb28904f021d9a6b27fc30051bb56b5fb0eb6a
Raynn <jfarmer0124@gmail.com>
2024-04-19 13:36:25 -0700

    Update collatz.js
    Refining stats; distribution isn't working yet.

--- docs/assets/js/collatz.js
+++ docs/assets/js/collatz.js
@@ -1,6 +1,6 @@
-let collatzArray = [];
-
 window.addEventListener('load', function() {
+	let collatzArray = [];
+	let stats = { maxSteps: { num: 1, steps: 0 }, stepDist: [], counter: 0 };
 	collatzArray[1] = { steps: 0, used: true, path: "<li class=\"oddNum\">1</li>" };
 
 	document.getElementById('pathButton').onclick = function(e) {
@@ -27,62 +27,72 @@ window.addEventListener('load', function() {
 	document.getElementById('statsButton').onclick = function(e) {
 		let val = document.getElementById('value').value;
 		let resultElement = document.getElementById('results');
-		let statsElement = document.getElementById('stats');
 		let resultStr = "";
-		let pathStr = "";
-		let statsStr = "";
 
-
-		pathElement.innerHTML = "";
 		resultElement.innerHTML = "";
-		statsElement.innerHTML = "<table id=\"statistics\"><thead><tr><th>Number</th><th>Steps</th></tr></thead><tbody id=\"statsBody\"></tbody></table>";
-
-		let statsBody = document.getBodyById('stats');
-		statsBody.innerHTML = "";
 
 		try {
-			statsStr = statistics(BigInt(val));
-			pathStr += collatzArray[num].path;
-	  		resultStr = "Total Steps: " + collatzArray[num].steps;
+			let num = BigInt(val);
+			if (num <= 0) throw new Error("Number must be a positive integer.");
+			resultStr = statistics(num);
 		} catch(e) {
 			resultStr = "<strong>Error:</strong> " + e.message;
-			statsElement.innerHTML = "";
 		} finally {
 			resultElement.innerHTML = resultStr;
-			resultElement.innerHTML += pathStr;
-			statsBody = statsStr;
 		}
 	}
-});
 
-function statistics(num) {
-	if (num > 10000) {
-		errorMessage("Number is too large for this function.");
-		return false;
+	function statistics(num) {
+		if (num > 10000) {
+			throw new Error("Number is too large for this function.");
+		}
+
+		nextStep(num);
+
+		let result = "";
+		let maxSteps = { num: 1, steps: 0 };
+		let counter = collatzArray.filter(function(el) { return el && el.used; }).length;
+		let stepDist = [];
+
+		for (let i = 1n; i <= num; i++) {
+			if (!collatzArray[i]) continue;  // Not every number is done yet
+
+			if (!stepDist[collatzArray[i].steps]) stepDist[collatzArray[i].steps] = 0;
+			stepDist[collatzArray[i].steps]++;
+
+			if (collatzArray[i].steps > maxSteps.steps) {
+				maxSteps.num = i;
+				maxSteps.steps = collatzArray[i].steps;
+			}
+		}
+
+		result += "<p>" + counter + " numbers calculated.</p>";
+		result += "<p>Maximum Steps: " + maxSteps.steps + " steps for " + maxSteps.num + "</p>";
+		result += "Distribution:<table><thead><tr><th>Steps</th><th>Times Occurred</th></tr></thead><tbody>";
+		for (i = 0; i <= maxSteps.steps; i++) {
+			if (stepDist[i]) {
+				result += "<tr><td>" + i + "</td><td>" + stepDist[i] + "</td></tr>";
+			}
+		}
+		result += "</tbody></table>";
+
+		return result;
 	}
 
-	let result = ""
-	for (let i = 1n; i <= num; i++) {
-		nextStep(i);
-		result = "<tr><td>" + i + "</td><td>" + collatzArray[i].steps + "</td></tr>" + result;
-	}
+	function nextStep(num) {
+		let isEven = num % 2n == 0n;
 
-	return result;
-}
+		if (collatzArray[num] && collatzArray[num].used) {
+			return collatzArray[num].steps;
+		}
 
-function nextStep(num) {
-	let isEven = num % 2n == 0n;
+		let nextNum = isEven ? (num/2n) : (3n * num + 1n);
 
-	if (collatzArray[num] && collatzArray[num].used) {
+		collatzArray[num] = { used: true };
+
+		collatzArray[num].steps = nextStep(nextNum) + 1;
+		collatzArray[num].path = "<li class=\""+ (isEven ? "evenNum" : "oddNum") + "\">" + num + "</li>" + collatzArray[nextNum].path;
+
 		return collatzArray[num].steps;
 	}
-
-	let nextNum = isEven ? (num/2n) : (3n * num + 1n);
-
-	collatzArray[num] = { used: true };
-
-	collatzArray[num].steps = nextStep(nextNum) + 1;
-	collatzArray[num].path = "<li class=\""+ (isEven ? "evenNum" : "oddNum") + "\">" + num + "</li>" + collatzArray[nextNum].path;
-
-	return collatzArray[num].steps;
-}
+});
